330a0f2df55ec421847ed4d529a4077e108fd3ce
Filipe Augusto <filipea133@gmail.com>
2016-11-02 10:17:39 -0200

    Primeiro commit - cenas criadas

---
+++ Assets/Scripts/PatidaUIController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PatidaUIController : MonoBehaviour {
+
+    public void Sair()
+    {
+        SceneManager.LoadScene("inicio");
+
+    }
+
+}
