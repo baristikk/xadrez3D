3ff4e562ccd1a8fb75b21d2241b9bfd7be10e476
FILIPE AUGUSTO SOUSA E SILVA <01890828602@est.udicentro.local>
2016-11-07 09:50:49 -0200

    Acrescentando e adaptando o codigo do projeto xadrez

---
+++ Assets/Scripts/tabuleiro/Cor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Azul, 
+        Vermelha,
+        Verde,
+        Laranja
+
+    }
+}
